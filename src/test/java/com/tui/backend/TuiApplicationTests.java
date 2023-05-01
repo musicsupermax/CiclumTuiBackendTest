cb7bdfd05117b0d47f10ac9d8be4f890cd58954b
maksymhavryk <maksym.havryk-external@sema4genomics.com>
2023-05-01 13:36:21 +0300

    init commit

---
+++ src/test/java/com/tui/backend/TuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tui.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
