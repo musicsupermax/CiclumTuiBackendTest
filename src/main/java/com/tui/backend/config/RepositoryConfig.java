3eebfa91a7d7264fca7538c2fc80f0f2bfad2ff2
maksymhavryk <maksym.havryk-external@sema4genomics.com>
2023-05-05 19:03:34 +0300

    fixed bean name conflict

--- src/main/java/com/tui/backend/config/RepositoryConfig.java
+++ src/main/java/com/tui/backend/config/RepositoryConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class RepositoryConfig {
 
     @Bean
-    public RepositoryService gitHubRepositoryService() {
+    public RepositoryService repositoryService() {
         return new RepositoryService();
     }
 
